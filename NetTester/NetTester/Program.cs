fb5962efeee09609ec32dec66e847ef75c801a94
Nickolas Reid <nickolaswdreid@gmail.com>
2013-08-25 19:40:14 -0300

    Net connectivity test
    Checks to see if you are connected to the internet, rather than going
    through command promt and typing it out.

---
+++ NetTester/NetTester/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation;
+
+namespace NetTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // resize window so it's not maximized
+            Console.SetWindowSize(50, 25);
+            
+            // Send out ping
+            Ping sending = new Ping();
+            // Options for the ping
+            PingOptions options = new PingOptions();
+            // Don't fragment the ping.
+            options.DontFragment = false;
+            // 32 bit message to be sent.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            // encode the data to bytes
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            // have a time out just in case
+            int timeout = 120;
+            // try pinging
+            try
+            {
+                PingReply reply = sending.Send("8.8.8.8", timeout, buffer, options);
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.Write("Success!");
+                }
+                else
+                {
+                    // network may be down.
+                    Console.Write("Connection failed");
+                }
+            }
+            catch (Exception)
+            {
+                // just a catch all
+                Console.Write("Host not found");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
