939bbe02988ea0510d99e182338ceec1eb615493
Nickolas Reid <nickolaswdreid@gmail.com>
2013-08-25 12:51:55 -0300

    Comments, code change
    - More commenting
    - Fixed Event Handler
    - Now checks for diagnols

--- Tic Tac Toe/Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -11,12 +11,16 @@ namespace Tic_Tac_Toe
 {
     public partial class GameForm : Form
     {
+        // Each tic tac toe button.
         Button[,] rows = new Button[3, 3];
+        // an array to show who is at which square/
         int[,] progress = new int[3, 3];
         public GameForm()
         {
             int height = 100, width = 100, horizontal = 0, vertical = 0;
+
             InitializeComponent();
+            // Creating the buttons for the game.
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
@@ -24,19 +28,22 @@ namespace Tic_Tac_Toe
                     rows[i, j] = new Button();
                     rows[i, j].Size = new Size(height, width);
                     rows[i, j].Location = new Point(horizontal, vertical);
-                    rows[i, j].Click += new EventHandler(Click);
+                    // Action to happen when a player clicks.
+                    rows[i, j].Click += new EventHandler(Clicked);
                     horizontal += 100;
-
+                    //Third line of buttons.
                     this.Controls.Add(rows[i, j]);
                     if (horizontal == 300 && vertical == 200)
                     {
                         horizontal = 0; 
                     }
+                    //Second line of buttons.
                     else if (horizontal == 300 && vertical == 100)
                     {
                         horizontal = 0;
                         vertical = 200;
                     }
+                    // first line of buttons.
                     else if (horizontal == 300)
                     {
                         horizontal = 0;
@@ -48,12 +55,36 @@ namespace Tic_Tac_Toe
             }
 
         }
-
+        // method for checking for a win.
         private void checkSquares()
         {
+            // check which player has won.
+
+            int compWin = 0, playerWin = 0;
+
+
+            // check for diagnols
+            if (progress[0, 0] == 1 && progress[1, 1] == 1 && progress[2, 2] == 1)
+            {
+                MessageBox.Show("Computer Wins!");
+            }
+            else if (progress[0, 2] == 1 && progress[1, 1] == 1 && progress[2, 0] == 1)
+            {
+                MessageBox.Show("Computer Wins!");
+            }
+            else if(progress[0, 0] == 2 && progress[1, 1] == 2 && progress[2, 2] == 2)
+            {
+                MessageBox.Show("Player Wins!");
+            }
+            else if (progress[0, 2] == 2 && progress[1, 1] == 2 && progress[2, 0] == 2)
+            {
+                MessageBox.Show("Player Wins!");
+            }
+
+            //Check for lines
             for (int i = 0; i < 3; i++)
             {
-                int compWin = 0,playerWin = 0;
+                
                 for (int j = 0; j < 3; j++)
                 {
                     if (progress[i, j] == 1)
@@ -81,7 +112,7 @@ namespace Tic_Tac_Toe
        
         }
         //new
-        private void Click(object sender, EventArgs args)
+        private void Clicked(object sender, EventArgs args)
         {
             Button clicked = (Button)sender;
             clicked.BackColor = Color.AliceBlue;
