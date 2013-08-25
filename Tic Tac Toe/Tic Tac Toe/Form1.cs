b9d34379bebe375146a0041e7fd8fe05ffae35c8
Nickolas Reid <nickolaswdreid@gmail.com>
2013-08-25 16:57:59 -0300

    Moved Code
    - Moved code around no real progress

--- Tic Tac Toe/Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -14,7 +14,17 @@ namespace Tic_Tac_Toe
         // Each tic tac toe button.
         Button[,] rows = new Button[3, 3];
         // an array to show who is at which square/
-        int[,] progress = new int[3, 3];
+        int[,] progress = new int[,] {
+            {0,1,2},
+            {3,4,5},
+            {6,7,8},
+            {0,3,6},
+            {1,4,7},
+            {2,5,8},
+            {0,4,8},
+            {2,4,6}
+        };
+
         public GameForm()
         {
             int height = 100, width = 100, horizontal = 0, vertical = 0;
@@ -28,6 +38,8 @@ namespace Tic_Tac_Toe
                     rows[i, j] = new Button();
                     rows[i, j].Size = new Size(height, width);
                     rows[i, j].Location = new Point(horizontal, vertical);
+                    rows[i, j].Text = "";
+                    
                     // Action to happen when a player clicks.
                     rows[i, j].Click += new EventHandler(Clicked);
                     horizontal += 100;
@@ -35,7 +47,7 @@ namespace Tic_Tac_Toe
                     this.Controls.Add(rows[i, j]);
                     if (horizontal == 300 && vertical == 200)
                     {
-                        horizontal = 0; 
+                        horizontal = 0;
                     }
                     //Second line of buttons.
                     else if (horizontal == 300 && vertical == 100)
@@ -51,139 +63,137 @@ namespace Tic_Tac_Toe
                     }
 
                 }
-                
+
             }
 
         }
         // method for checking for a win.
-        private void checkSquares()
-        {
-            // check which player has won.
+        //private void checkSquares()
+        //{
+        //    // check which player has won.
 
-            int compWin = 0, playerWin = 0;
+        //    int compWin = 0, playerWin = 0;
 
 
-            // check for diagnols
-            if (progress[0, 0] == 1 && progress[1, 1] == 1 && progress[2, 2] == 1)
-            {
-                MessageBox.Show("Computer Wins!");
-                BoardFreeze();
-            }
-            else if (progress[0, 2] == 1 && progress[1, 1] == 1 && progress[2, 0] == 1)
-            {
-                MessageBox.Show("Computer Wins!");
-                BoardFreeze();
-            }
-            else if(progress[0, 0] == 2 && progress[1, 1] == 2 && progress[2, 2] == 2)
-            {
-                MessageBox.Show("Player Wins!");
-                BoardFreeze();
-            }
-            else if (progress[0, 2] == 2 && progress[1, 1] == 2 && progress[2, 0] == 2)
-            {
-                MessageBox.Show("Player Wins!");
-                BoardFreeze();
-            }
+        //    // check for diagnols
+        //    if (progress[0, 0] == 1 && progress[1, 1] == 1 && progress[2, 2] == 1)
+        //    {
+        //        MessageBox.Show("Computer Wins!");
+        //        BoardFreeze();
+        //    }
+        //    else if (progress[0, 2] == 1 && progress[1, 1] == 1 && progress[2, 0] == 1)
+        //    {
+        //        MessageBox.Show("Computer Wins!");
+        //        BoardFreeze();
+        //    }
+        //    else if (progress[0, 0] == 2 && progress[1, 1] == 2 && progress[2, 2] == 2)
+        //    {
+        //        MessageBox.Show("Player Wins!");
+        //        BoardFreeze();
+        //    }
+        //    else if (progress[0, 2] == 2 && progress[1, 1] == 2 && progress[2, 0] == 2)
+        //    {
+        //        MessageBox.Show("Player Wins!");
+        //        BoardFreeze();
+        //    }
 
-            //Check for lines
-            for (int i = 0; i < 3; i++)
-            {
-                
-                for (int j = 0; j < 3; j++)
-                {
-                    if (progress[i, j] == 1)
-                    {
-                        compWin++;
-                    }
-                    else if (progress[i, j] == 2)
-                    {
-                        playerWin++;
-                    }
-                    if (compWin == 3)
-                    {
-                        MessageBox.Show("Computer Wins!");
-                        BoardFreeze();
-                        compWin = 0;
+        //    //Check for lines
+        //    for (int i = 0; i < 3; i++)
+        //    {
 
-                    }
-                    else if (playerWin == 3)
-                    {
-                        MessageBox.Show("You win!");
-                        BoardFreeze();
-                        playerWin = 0;
-                    }
-                }
-                
-            }
-       
-        }
-        //new
+        //        for (int j = 0; j < 3; j++)
+        //        {
+        //            if (progress[i, j] == 1)
+        //            {
+        //                compWin++;
+        //            }
+        //            else if (progress[i, j] == 2)
+        //            {
+        //                playerWin++;
+        //            }
+        //            if (compWin == 3)
+        //            {
+        //                MessageBox.Show("Computer Wins!");
+        //                BoardFreeze();
+        //                compWin = 0;
+
+        //            }
+        //            else if (playerWin == 3)
+        //            {
+        //                MessageBox.Show("You win!");
+        //                BoardFreeze();
+        //                playerWin = 0;
+        //            }
+        //            compWin = 0;
+        //            playerWin = 0;
+        //        }
+
+        //    }
+
+        //}
+        // Places player piece on board.
         private void Clicked(object sender, EventArgs args)
         {
             Button clicked = (Button)sender;
+            
 
-
-            if (clicked.BackColor != Color.AliceBlue && clicked.BackColor != Color.Beige)
+            if (clicked.BackColor == SystemColors.Control)
             {
                 clicked.BackColor = Color.AliceBlue;
+                clicked.Enabled = false;
                 CheckedIfClicked();
-                checkSquares();
+               // checkSquares();
+                
             }
             ComputerTurn();
 
- 
-       }
+
+        }
+        // Method to put the players piece in an array
         private void CheckedIfClicked()
         {
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if(rows[i,j].BackColor == Color.AliceBlue){
+                    if (rows[i, j].BackColor == Color.AliceBlue)
+                    {
                         progress[i, j] = 2;
                     }
                 }
             }
         }
+        // Freezes the board at game over.
         private void BoardFreeze()
         {
             for (int i = 0; i < 3; i++)
             {
-                for (int j = 0;j < 3; j++){
-                    rows[i,j].Enabled = false;
+                for (int j = 0; j < 3; j++)
+                {
+                    rows[i, j].Enabled = false;
                 }
             }
         }
+        // Let's the computer make a move.
         private void ComputerTurn()
-        {
-            Random H = new Random();
-
-            int Hor = H.Next(3);
-
-            int Ver = H.Next(3);
-
-
-            while (rows[Hor, Ver].BackColor == SystemColors.Control)
-            {
-
+        {         
+            Random R = new Random();
+            int Hor = R.Next(3);
+            int Ver = R.Next(3);
+          
+            
                 if (rows[Hor, Ver].BackColor == SystemColors.Control)
                 {
+
                     rows[Hor, Ver].BackColor = Color.Beige;
                     progress[Hor, Ver] = 1;
                     rows[Hor, Ver].Enabled = false;
-                    
+        
                 }
                 else
                 {
-                    H.Next(3);
+                        ComputerTurn();
                 }
-
-            }
-
-
-
         }
-
-
     }
 }
