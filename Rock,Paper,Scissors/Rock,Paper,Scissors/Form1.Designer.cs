9656ef30d2f0828cd44a641c3650f15fc5e2d08a
Nickolas Reid <nickolaswdreid@gmail.com>
2013-08-25 18:06:23 -0300

    V 1.0
    - Plays game
    - counts
    - Starting to add music.

---
+++ Rock,Paper,Scissors/Rock,Paper,Scissors/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CoinFlip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRockPaperSciccors = new System.Windows.Forms.Button();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnSciccors = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRockPaperSciccors
+            // 
+            this.btnRockPaperSciccors.Location = new System.Drawing.Point(223, 103);
+            this.btnRockPaperSciccors.Name = "btnRockPaperSciccors";
+            this.btnRockPaperSciccors.Size = new System.Drawing.Size(110, 97);
+            this.btnRockPaperSciccors.TabIndex = 0;
+            this.btnRockPaperSciccors.UseVisualStyleBackColor = true;
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(12, 12);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(75, 85);
+            this.btnRock.TabIndex = 1;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Location = new System.Drawing.Point(12, 103);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(75, 86);
+            this.btnPaper.TabIndex = 2;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnSciccors
+            // 
+            this.btnSciccors.Location = new System.Drawing.Point(12, 195);
+            this.btnSciccors.Name = "btnSciccors";
+            this.btnSciccors.Size = new System.Drawing.Size(75, 86);
+            this.btnSciccors.TabIndex = 3;
+            this.btnSciccors.Text = "Scissors";
+            this.btnSciccors.UseVisualStyleBackColor = true;
+            this.btnSciccors.Click += new System.EventHandler(this.btnSciccors_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(93, 103);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(87, 25);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "Score: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 290);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnSciccors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.btnRockPaperSciccors);
+            this.Name = "Form1";
+            this.Text = "Rock,Paper,Scissors";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRockPaperSciccors;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnSciccors;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ Rock,Paper,Scissors/Rock,Paper,Scissors/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CoinFlip
+{
+    public partial class Form1 : Form
+    {
+        int userDecision;
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            System.Media.SoundPlayer sp = new System.Media.SoundPlayer(@"C:\Users\McAwesome\Documents\CoffeeCup Software\Sounds\cash.wav");
+            sp.Play();
+        }
+
+        private void CompDecision()
+        {
+            Random R = new Random();
+            int decision = R.Next(3);
+            
+            State(decision);
+
+
+            if (userDecision == decision)
+            {
+                lblScore.Text = "Tie!";
+            }
+            else if (userDecision == 0 && decision == 1)
+            {
+                
+                score--;
+                Score();
+            }
+            else if (userDecision == 1 && decision == 0)
+            {
+                
+                score++;
+                Score();
+            }
+            else if (userDecision == 1 && decision == 2)
+            {
+                
+                score--;
+                Score();
+            }
+            else if (userDecision == 2 && decision == 1)
+            {
+                
+                score++;
+                Score();
+            }
+            else if (userDecision == 0 && decision == 2)
+            {
+                
+                score++;
+                Score();
+            }
+            else if (userDecision == 2 && decision == 0)
+            {
+                
+                score--;
+                Score();
+            }
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            userDecision = 0;
+            CompDecision();
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            userDecision = 1;
+            CompDecision();
+        }
+
+        private void btnSciccors_Click(object sender, EventArgs e)
+        {
+            userDecision = 2;
+            CompDecision();
+        }
+        private void State(int state)
+        {
+            if (state == 0)
+            {
+                btnRockPaperSciccors.Text = "Rock";
+            }
+            else if (state == 1)
+            {
+                btnRockPaperSciccors.Text = "Paper";
+            }
+            else
+            {
+                btnRockPaperSciccors.Text = "Scissors";
+            }
+        }
+        private void Score()
+        {
+            lblScore.Text = "Score: " + score.ToString();
+        }
+    }
+}
